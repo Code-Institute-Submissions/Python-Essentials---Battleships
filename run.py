97fb706ad64702fd7c0e846c0ff750c01aaed152
WHLW27 <will.lord.wilson@gmail.com>
2022-10-17 10:48:50 +0000

    annotated setGridLevel function

--- run.py
+++ run.py
@@ -5,21 +5,31 @@ grid_size = 0
 # Global variable for the grid level
 grid_level = 0
 
+
+# the function below will set the game grid size
 def setGridLevel(): 
     global grid_level
     
     while True:
         try:
+            # parse the user input as an integer assign this to the grid_level variable.
             grid_level = int(input(f"Enter a whole number between 5 and 20 to set the difficulty.\nThe number input shall set the size of the games grid.\n"))
+            # check if input is between 5 and 20 if yes continue otherwise print error. 
+
             if grid_level >= 5 and grid_level <= 20:
                 print("Setting difficulty...")
+
             else:
                 print(f"Your input is not within the range, please input an number between 5 and 20.\n")
                 continue
+            
+        # print valueError is input is not a an integer and restart loop
         except ValueError:
             print(f"Your input is not valid, please input an integer, e.g. a whole number between 5 and 20.\n")
             continue
+
         else:
+            # if input is valid, print chosen level and return false to close the while loop
             print(f"You selected Level {grid_level}. Game Ready.")
             return False
 
