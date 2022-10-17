30cbc19ce3aaf580b7cf0cd5692eec80d0e05d75
WHLW27 <will.lord.wilson@gmail.com>
2022-10-17 10:24:27 +0000

    added a check to setgridlevel to check if input is an int

--- run.py
+++ run.py
@@ -7,11 +7,22 @@ grid_level = 0
 
 def setGridLevel(): 
     global grid_level
-    grid_level = input(f"Enter a whole number up to 15 to set the grid level\n")
-    print(f"You have chosen level {grid_level}. Good Luck!\n")
+    
+    while True:
+        try:
+            grid_level = int(input(f"Enter a whole number up to 15 to set the grid level\n"))
+        except ValueError:
+            print(f"Your input is not valid, please input an integer.\n")
+            continue
+        else:
+            print(f"You chose Level {grid_level}")
 
-def printGridLevel():
-    print(grid_level)
+            return False
+
+    if grid_level >= 5 and grid_level <= 20:
+        print(f"You have chosen level {grid_level}. Good Luck!\n")
+    else:
+        print(f"Your input is not valid, please input an integer between 5 and 20.\n")
+        setGridLevel()
 
 setGridLevel()
-printGridLevel()
