963e074d782ff9fc26da2c33c72103d83ba7a6f8
WHLW27 <will.lord.wilson@gmail.com>
2022-10-17 08:56:47 +0000

    Added globals for grid size, added function to set grid level through user input

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Global variable for the grid
+game_grid = [[]]
+# Global variable for the grid size
+grid_size = 0
+# Global variable for the grid level
+grid_level = 0
+
+def setGridLevel(): 
+    global grid_level
+    grid_level = input(f"Enter a whole number up to 15 to set the grid level\n")
+    print(f"You have chosen level {grid_level}. Good Luck!\n")
+
+def printGridLevel():
+    print(grid_level)
+
+setGridLevel()
+printGridLevel()
