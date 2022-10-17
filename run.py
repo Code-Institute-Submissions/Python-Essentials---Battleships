1501263408b92d0349c9e0ab17bd78fd6fae4b08
WHLW27 <will.lord.wilson@gmail.com>
2022-10-17 14:21:46 +0000

    minor code adjustments

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def setGridLevel():
             # check if input is between 5 and 20 if yes continue otherwise print error. 
 
             if grid_level >= 5 and grid_level <= 20:
-                print("Setting difficulty...")
+                print(f"Setting difficulty...\n")
 
             else:
                 print(f"Your input is not within the range, please input an number between 5 and 20.\n")
@@ -32,25 +32,30 @@ def setGridLevel():
 
         else:
             # if input is valid, print chosen level and return false to close the while loop
-            print(f"You selected Level {grid_level}. Game Ready.")
+            print(f"You selected Level {grid_level}. Game Ready.\n")
             return False
+
 
 # the function below shall compose and print a grid set to the size input by the user
 def make_grid(ocean_wave, grid_level):
     # set rows as empty strings
     header_row = ''
-    row = ''
+    standard_row = ''
     # compose the rows and seperating characters, loop as many times as stated in grid_level +1 to ensure 5 playable rows are printed
-    for x in range(1,grid_level+1):
-        header_row = header_row + '|' + str(x)
-        row = row + '|' + ocean_wave 
+    for num in range(1,grid_level+1):
+        # creates the header numbers
+        header_row = header_row + '|' + str(num)
+        # creates the playing area
+        standard_row = standard_row + '|' + ocean_wave 
     print(header_row + '|')
-    print('-' * (len(row)+1))
-    
+    print('-' * (len(standard_row)+1))
+    # Apply alphabetical characters to the rows for coordiantion
     char = 64
-    for x in range(1,grid_level):
+    for x in range(1,grid_level+1):
         char = char  +1 
-        print(chr(char) + row + '|')
+        print(chr(char) + standard_row + '|')
+    print(f" BOMBS AHOY!\n")
+
 
 # main function
 def main():
