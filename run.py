c1f0cb2eace2783c33b72616e172a62aeae09f0b
WHLW27 <will.lord.wilson@gmail.com>
2022-10-17 14:17:41 +0000

    added function to compose and print a grid with alpha numerical cordinates set to the returned grid_level

--- run.py
+++ run.py
@@ -35,21 +35,26 @@ def setGridLevel():
             print(f"You selected Level {grid_level}. Game Ready.")
             return False
 
-
-setGridLevel()
-
+# the function below shall compose and print a grid set to the size input by the user
 def make_grid(ocean_wave, grid_level):
+    # set rows as empty strings
     header_row = ''
     row = ''
+    # compose the rows and seperating characters, loop as many times as stated in grid_level +1 to ensure 5 playable rows are printed
     for x in range(1,grid_level+1):
         header_row = header_row + '|' + str(x)
         row = row + '|' + ocean_wave 
     print(header_row + '|')
     print('-' * (len(row)+1))
+    
     char = 64
-    for x in range(1,grid_level+1):
+    for x in range(1,grid_level):
         char = char  +1 
         print(chr(char) + row + '|')
 
+# main function
+def main():
+    setGridLevel()
+    make_grid(ocean_wave, grid_level)
 
-make_grid(ocean_wave, grid_level)
+main()
