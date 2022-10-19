32649acbdf7a60c3fe595d9a33ecfe6a64096e4f
WHLW27 <will.lord.wilson@gmail.com>
2022-10-19 09:47:53 +0000

    altered functions to not rely on global variables, changes to structure of script for readability

--- run.py
+++ run.py
@@ -1,16 +1,14 @@
 import random
 import snoop
 
-# Global variable for the grid
-game_grid = [[]]
-# Global variable for the grid copy to track ships 
-hit_tracker = [[]]
-# Global variable for the grid size
-grid_size = 0
-# Global variable for the grid level
-grid_level = 0
-# Global variable for the untargeted grid coordiantes
-ocean_wave = "~"
+# # Global variable for the grid
+# game_grid = [[]]
+# # Global variable for the grid copy to track ships 
+# hit_tracker = [[]]
+# # Global variable for the grid size
+# grid_size = 0
+# # Global variable for the grid level
+# grid_level = 0
 
 
 def setGridLevel(): 
@@ -21,7 +19,7 @@ def setGridLevel():
     The code will validate the user input by checking if the figure is an integer, equal to 
     or less then 3 and equal too or less than 10.
     """
-    global grid_level
+    grid_level = 0
     
     while True:
         try:
@@ -31,7 +29,7 @@ def setGridLevel():
 
             if grid_level >= 3 and grid_level <= 10:
                 print(f"Setting difficulty...\n")
-                return grid_level
+                make_grid(grid_level)
 
             else:
                 print(f"Your input is not within the range, please input an number between 3 and 10.\n")
@@ -48,58 +46,30 @@ def setGridLevel():
             return False
 
 
-def make_grid():
+def make_grid(grid_level):
     """
     This function will build a sequence of lists which shall then be used,
     later in this script to print the playing area of the game to the terminal.
     """
-    global game_grid
-    global hit_tracker
+    game_grid = [[]]
+    hit_tracker = [[]]
     # set rows and columns to the returned grid_level int.
     rows, columns = (grid_level, grid_level)
-
-    # Create an empty list for the playing area
-    game_grid = []
 
     # loop through the number of rows 
     for row in range(rows):
         row = []
         # for every column append a wave 
         for col in range(columns):
-            row.append(ocean_wave)
+            row.append("~")
         # append the return lists to the game_grid variable
         game_grid.append(row)
         hit_tracker.append(row)
-
-def print_play_area():
-    # global game_grid
-    debug_mode = False
-    # create a string of the alphabet to be used as coordiantes
-    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    print("   BOMBS AHOY!")
-    # for each row within game grid, print the corresponding letter.
-    for row in range(len(game_grid)):
-        print(characters[row], end="| ")
-        # then for every column in game_grid print the symbol representing a wave
-        for col in range(len(game_grid[row])):
-            # this code will check for the placement of a ship 
-            if game_grid[row][col] == "O":
-                if debug_mode:
-                    print("O", end=" ")
-                else:
-                    print(ocean_wave, end=" ")
-            else:
-                print(game_grid[row][col], end=" ")
-        print("")
-
-    print("  ", end=" ")
-    for num in range(len(game_grid)):
-        print(str(num), end=" ")
-    print("")
+    build_ships(grid_level, game_grid, hit_tracker)
+    print_play_area(game_grid)
 
 
-def build_ships():
-    global grid_level
+def build_ships(grid_level, game_grid, hit_tracker):
 
     ships_to_place = 0
     enemy_counter = 0
@@ -121,16 +91,41 @@ def build_ships():
             ship_size = random.randint(1, 4)
         elif grid_level > 8:
             ship_size = random.randint(1, 5)
-        if place_ship(latitude, longitude, heading, ship_size):
+        if place_ship(latitude, longitude, heading, ship_size, grid_level, game_grid, hit_tracker):
             enemy_counter += 1
 
+
+def print_play_area(game_grid):
+    debug_mode = True
+    # create a string of the alphabet to be used as coordiantes
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    print("   BOMBS AHOY!")
+    # for each row within game grid, print the corresponding letter.
+    for row in range(len(game_grid)):
+        print(characters[row], end="| ")
+        # then for every column in game_grid print the symbol representing a wave
+        for col in range(len(game_grid[row])):
+            # this code will check for the placement of a ship 
+            if game_grid[row][col] == "O":
+                if debug_mode:
+                    print("O", end=" ")
+                else:
+                    print("~", end=" ")
+            else:
+                print(game_grid[row][col], end=" ")
+        print("")
+
+    print("  ", end=" ")
+    for num in range(len(game_grid)):
+        print(str(num), end=" ")
+    print("")
 
 
 def main():
     setGridLevel()
-    make_grid()
-    build_ships()
-    print_play_area()
+    # make_grid()
+    # # build_ships()
+    # print_play_area()
     
 
 main()
